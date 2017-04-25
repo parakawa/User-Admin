70ba16babfe51da9c6f2c7c4713d772e497f50a9
Paty Arakawa <paty.arakawa@gmail.com>
2017-04-25 01:11:47 -0500

    add procfile

--- controllers/web_controller.js
+++ controllers/web_controller.js
@@ -1,3 +1,10 @@
 exports.index = function (req, res){
 	res.render('index');
 };
+
+exports.admin = function (req, res){
+	models.User.find(function(err, users){
+		if(err){console.log(err);}
+		res.render('admin/index', {users: users });
+	});
+};
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var webController = require('../controllers/web_controller');
 var loginController = require('../controllers/login_controller');
 
 router.get("/", webController.index);
+router.get("/admin", webController.admin);
 router.post('/signUp', loginController.signUp);
 router.post('/login', loginController.login);
 
